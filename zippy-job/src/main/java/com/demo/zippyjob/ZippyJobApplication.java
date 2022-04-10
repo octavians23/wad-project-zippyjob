fe71bfeb90de1d04c5896e8d7570382ddae87787
octavians23 <stoica.octavian17@gmail.com>
2022-04-10 15:59:40 +0300

    First commit

---
+++ zippy-job/src/main/java/com/demo/zippyjob/ZippyJobApplication.java
@@ -0,0 +1,13 @@
+package com.demo.zippyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippyJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZippyJobApplication.class, args);
+    }
+
+}
