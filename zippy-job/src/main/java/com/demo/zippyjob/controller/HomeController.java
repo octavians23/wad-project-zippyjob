d65bcfe7a399aaf7598e11aca7f4696e9e424d47
octavians23 <stoica.octavian17@gmail.com>
2022-04-13 16:16:32 +0300

    added login and register pages and routes

---
+++ zippy-job/src/main/java/com/demo/zippyjob/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.demo.zippyjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
